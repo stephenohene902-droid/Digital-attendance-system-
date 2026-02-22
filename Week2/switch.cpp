b7ab21891b1ab61f0316e85e96575e753c796577
stephenohene902-droid <stephenohene902@gmail.com>
2026-02-22 17:53:28 +0000

    Added switch statement program

---
+++ Week2/switch.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day;
+
+    cout << "Enter a number (1-7) for the day of the week: ";
+    cin >> day;
+
+    switch(day) {
+        case 1:
+            cout << "Sunday";
+            break;
+        case 2:
+            cout << "Monday";
+            break;
+        case 3:
+            cout << "Tuesday";
+            break;
+        case 4:
+            cout << "Wednesday";
+            break;
+        case 5:
+            cout << "Thursday";
+            break;
+        case 6:
+            cout << "Friday";
+            break;
+        case 7:
+            cout << "Saturday";
+            break;
+        default:
+            cout << "Invalid number";
+    }
+
+    return 0;
+}
